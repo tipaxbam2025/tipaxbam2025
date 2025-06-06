efe2e250c83489d82fc9f9ee13d7c2bd39d73e01
tipaxbam2025 <masoudbaxm@gmail.com>
2025-06-06 05:04:20 +0330

    tipaxbam2025

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "tipaxbam2025"         # 用户名
+UUID = "79420d97-6f83-40d6-b957-900c1742e9ae"                     # UUID，留空则自动生成
+PORT = 46777                   # Vmess端口，留空或0则自动生成
+DOMAIN = "royabax.ir"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYzYxNjFlNzlkM2EyYzJiM2QzMGQxZGNkMWQ0MjllNjQiLCJ0IjoiOGMxMzk3YjUtMTYyOS00MWRkLTllOTYtZjY0ZjlkOGVjZjQ2IiwicyI6IllqaG1NMlkxWldVdFlUZ3pPQzAwTWpCaExUazJZbVF0TURObVpHRmtOalJsWTJGbCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
